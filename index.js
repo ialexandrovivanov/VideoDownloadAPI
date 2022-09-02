9733e9a05183ab3d257d572bbca569ae82391c4a
Ivan Alexandrov <ialexandrovivanov@gmail.com>
2022-09-02 18:33:05 +0300

    texts improved

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const keys = require(__dirname + "/keys.json");
 const express = require("express");
 const app = express();
 
-app.use(express.json());  // needed for parsing json objects from client
+app.use(express.json());  // parsing json objects from client
 app.use(express.urlencoded({ extended: true }));
+
+// ROUTING
 
 app.get("/", (req, res) => { 
     
@@ -16,23 +18,19 @@ app.get("/", (req, res) => {
 app.post("/", async (req, res) => {
     
     const validCaptcha = await validateCaptcha(req);    
-    if (!validCaptcha) { renderCaptchaPage(res); return; }  // validate google captcha
+    if (!validCaptcha) { renderCaptchaPage(res); return; }  
     const validUrl = await validateUrl(req);
-    if (!validUrl) { renderUnablePage(res); return; }       // validate provided link        
-    const videoFileName = await getVideoFileName(req);      // gets requested file from file system or undefined    
-    if (videoFileName) renderVideoPage(res, videoFileName); // video is already on server 
-    else {                                                  // new download
+    if (!validUrl) { renderUnablePage(res); return; }               
+    const videoFileName = await getVideoFileName(req);          
+    if (videoFileName) renderVideoPage(res, videoFileName); 
+    else {                                                  
         const success =  await downloadFile(req); 
         if(success) {
-            const videoFileName = await getVideoFileName(req); // newly created file could be mp4 or mkv
-            if (videoFileName) {
-                //await clearUserInputForm();
-                renderVideoPage(res, videoFileName);        // render video page with the name of the video file
-            }
-            else 
-            renderUnablePage(res);
+            const videoFileName = await getVideoFileName(req); 
+            if (videoFileName) renderVideoPage(res, videoFileName);        
+            else renderUnablePage(res);
         } 
-        else renderUnablePage(res);                         // if error when download render unable page
+        else renderUnablePage(res);                         
     }
 });
 
@@ -44,11 +42,6 @@ app.get("/video", (req, res) => {
 app.get("/icon", (req, res) => {
 
     res.sendFile(__dirname + "/favicon.ico");
-});
-
-app.get("/badlink", (req, res) => {
-
-    res.sendFile(__dirname + "/unable.html");
 });
 
 app.listen(80, () => { 
@@ -63,13 +56,13 @@ async function getVideoFileName(req, res) {
 
     try {
 
-        const allFiles = await getFiles(); // array of files with extensions (full names)
-        // returns full name of the first file which starts with requested id or undefined
-        return allFiles.find(f => f.startsWith(/v=(.*)/.exec(req.body.link)[1])); // first wich starts with id
+        const allFiles = await getFiles();
+        const file = allFiles.find(f => f.startsWith(/v=(.*)/.exec(req.body.link)[1]));
+        return file;
     } 
     catch (err) {
 
-        logger(err);
+        await logger(err);
         return undefined;
     }
 }
@@ -101,7 +94,6 @@ async function validateUrl(req) {
 async function renderVideoPage(res, videoFileName) {
 
     const original = await fs.readFileSync("video.html", "utf8");
-    // dynamicaly inserts requested file as query parameter to video source 
     const rendered = original.replace(/src="(.*)"/, `src="/video?filename=${videoFileName}"`); 
     res.writeHead(200, {'Content-type' : 'text/html'});
     res.write(rendered);
@@ -125,15 +117,18 @@ async function renderCaptchaPage(res) {
 
 async function downloadFile(req) {
 
+    await updateItdl();
+
     try {
 
-        childProcess.execSync(`ytdl --id ${req.body.link}`, async (err, stdout, stderr) => 
-            { await logger(err, stdout, stderr); });
+        childProcess.execSync(`ytdl --id ${req.body.link}`, async (err, stdout, stderr) => { 
+            await logger(err, stdout, stderr); 
+        });
         return true;
     } 
     catch (err) { 
 
-        logger(err); 
+        await logger(err); 
         return false; 
     }
 }
@@ -141,4 +136,20 @@ async function downloadFile(req) {
 async function logger(err, stdout, stderr) {
     
     console.log(err, stdout, stderr);
+}
+
+async function updateItdl() {
+
+    try {
+
+        childProcess.execSync("ytdl --update", async (err, stdout, stderr) => { 
+            await logger(err, stdout, stderr); 
+        });
+        return true;
+    } 
+    catch (err) { 
+
+        await logger(err); 
+        return false; 
+    }    
 }
